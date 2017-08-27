50fcecc44c6ea053ffb8e33cf5ffe4c63f347baa
Derwaan <derwaan@gmail.com>
2017-08-27 21:42:51 +0200

    Add new version of sendFile (instead of sendfile)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require('express')
 let app = express()
 
 app.get('/', (req, res) => {
-	res.sendfile(__dirname + '/public/index.html')
+	res.sendFile(__dirname + '/public/index.html')
 })
 
 app.use(express.static(__dirname + '/public'))
