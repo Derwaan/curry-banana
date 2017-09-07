1713885b088f14ed552d39669affa49d25949fdd
Derwaan <derwaan@gmail.com>
2017-09-07 10:33:55 +0200

    Add add function to reward
    Add eslint

--- public/js/reward.js
+++ public/js/reward.js
@@ -1,53 +1,42 @@
-$(() => {
-	const default_rewards = [
-		{
-			msg: 'Eat a delicious snack',
-			id: 0,
-		},
-		{
-			msg: 'Watch an episode of your favorite show',
-			id: 1,
-		},
-		{
-			msg: 'Tonight, relax in front of a move',
-			id: 2,
-		},
-		{
-			msg: 'You got 5 minutes on social media. Go !',
-			id: 3,
-		},
-		{
-			msg: 'For your next break, get 15 more minutes',
-			id: 4,
-		},
-	]
+/* global rewards Cookies */
 
-	var rewards_cookie = Cookies.get('rewards')
-	if(typeof rewards_cookie === "undefined") {
-		Cookies.set('rewards', JSON.stringify(default_rewards))
-		rewards_cookie = JSON.stringify(default_rewards)
+// Default rewards in there is no cookie
+const default_rewards = [
+	{
+		msg: 'Eat a delicious snack',
+		id: 0,
+	},
+	{
+		msg: 'Watch an episode of your favorite show',
+		id: 1,
+	},
+	{
+		msg: 'Tonight, relax in front of a move',
+		id: 2,
+	},
+	{
+		msg: 'You got 5 minutes on social media. Go !',
+		id: 3,
+	},
+	{
+		msg: 'For your next break, get 15 more minutes',
+		id: 4,
+	},
+]
+
+
+/*
+ * Fill the rewards list with rewards
+ * If there is no reward, add default message
+ */
+function fillRewards() {
+	if(rewards.length === 0) {
+		$("#rewards").append(`
+			<li>
+				There is no reward yet... Add one !				
+			</li>
+		`)
 	}
-	var rewards = JSON.parse(rewards_cookie)
-
-	fillRewards(rewards)
-
-	$('.remove_reward').click(function(event) {
-
-		for(var i = 0; i < rewards.length; i++) {
-			var reward = rewards[i];
-
-			if(reward.id == event.target.id.split('_')[1]) {
-				rewards.splice(i, 1);
-			}
-		}
-		Cookies.set('rewards', JSON.stringify(rewards))
-		$("#rewards").empty()
-		fillRewards(rewards)
-
-	})
-})
-
-function fillRewards(rewards) {
 	rewards.forEach((reward) => {
 		$("#rewards").append(`
 			<li>
@@ -57,3 +46,65 @@ function fillRewards(rewards) {
 	})
 }
 
+/*
+ * Find next free id
+ */
+function nextId() {
+	return new Date().getTime()
+}
+
+/*
+ * Save the cookie and reload rewards
+ */
+function saveCookie() {
+	Cookies.set('rewards', JSON.stringify(rewards))
+	$("#rewards").empty()
+	fillRewards()
+}
+
+/*
+ * Add a new reward to the rewards array
+ */
+function addReward(msg) {
+	var reward = {
+		msg: msg,
+		id: nextId()
+	}
+	rewards.push(reward)
+	saveCookie()
+}
+
+$(() => {
+
+	var rewards_cookie = Cookies.get('rewards')
+	if(typeof rewards_cookie === "undefined") {
+		Cookies.set('rewards', JSON.stringify(default_rewards))
+		rewards_cookie = JSON.stringify(default_rewards)
+	}
+	rewards = JSON.parse(rewards_cookie)
+
+	fillRewards()
+
+	$('#rewards').on('click','.remove_reward', (event) => {
+		for(var i = 0; i < rewards.length; i++) {
+			var reward = rewards[i];
+
+			if(reward.id == event.target.id.split('_')[1]) {
+				rewards.splice(i, 1);
+			}
+		}
+
+		saveCookie()
+
+	})
+
+	$('#add_reward').on('keypress', (event) => {
+		if(event.which === 13) {
+			var msg = $("#add_reward").val()
+			if(msg === "")
+				return
+			$("#add_reward").val("")
+			addReward(msg)
+		}
+	})
+})
